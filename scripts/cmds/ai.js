296ee42f743d543c3e8db0a609539987abe1b628
Abnonymou <168055650+Abnonymou@users.noreply.github.com>
2024-05-05 09:49:57 +0800

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -29,7 +29,7 @@ module.exports = {
       }
       const prompt = event.body.substring(prefix.length).trim();
    if (!prompt) {
-        await message.reply("Bonjour, comment puis-je vous aidez ? 💖");
+        await message.reply("yess how can I help you today?");
         return;
       }
 
